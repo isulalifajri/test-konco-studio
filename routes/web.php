bccbf1d192d725dac3cbcc6f1313fb2556649618
isulalifajri <isulalifajri@gmail.com>
2024-06-18 16:09:52 +0700

    start test konco studio

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function() {
+    return view('frontend.page.home', [
+        'title' => 'Halaman Home',
+    ]);
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthenticationController::class, 'login'])->name('login');
+    Route::post('/authenticate', [AuthenticationController::class, 'authenticate'])->name('authenticate');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+});
